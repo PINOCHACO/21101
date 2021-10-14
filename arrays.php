265d80b75d86a8b57208708baf2e9c6ac55023e7
PINOCHACO <83979346+PINOCHACO@users.noreply.github.com>
2021-10-14 09:47:18 +0200

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<?php
+array(
+    [vuelta 1] => array
+    (
+        [interacion => 0] cosa en posicion 0-0]
+        [interacion => 1] cosa en posicion 0-0]
+        [interacion => 2] cosa en posicion 0-0]
+    )
+)
+
